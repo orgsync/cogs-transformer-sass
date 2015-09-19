2032a3a23adf73784d2288823bafb87a170569d2
Casey Foster <c@sey.me>
2015-09-19 09:12:07 -0500

    node-sass@3.3.3, 1.0.4

--- index.js
+++ index.js
@@ -19,18 +19,15 @@ module.exports = function (file, options, cb) {
     data: file.buffer.toString() + ' ',
 
     // Always concat the file path so relative @imports work correctly.
-    includePaths: options.includePaths.concat(path.dirname(file.path)),
-
-    success: function (res) {
-      var links = _.map(res.stats.includedFiles, getRelative);
-      cb(null, {buffer: new Buffer(res.css), links: file.links.concat(links)});
-    },
-
-    error: function (er) {
-      cb(_.extend(new Error(), er, {
+    includePaths: options.includePaths.concat(path.dirname(file.path))
+  }), function (er, res) {
+    if (er) {
+      return cb(_.extend(new Error(), er, {
         message: file.path + ': line ' + er.line + ', column ' + er.column +
           ', ' + er.message
       }));
     }
-  }));
+    var links = _.map(res.stats.includedFiles, getRelative);
+    cb(null, {buffer: new Buffer(res.css), links: file.links.concat(links)});
+  });
 };
